a519ed4373133679761b036672b3ccb32f187663
kresna59 <agungkarisna@gmail.com>
2025-06-12 20:58:16 +0700

    formmainmenu
    gui menu

---
+++ ConsoleApp2/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ConsoleApp2
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnStartQuiz = new System.Windows.Forms.Button();
+            this.btnAddQuestion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(200, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(400, 50);
+            this.lblTitle.Text = "Welcome to QuizApp";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubtitle
+            // 
+            this.lblSubtitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblSubtitle.Location = new System.Drawing.Point(200, 80);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(400, 30);
+            this.lblSubtitle.Text = "Pertamina Quiz";
+            this.lblSubtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox
+            // 
+            //this.pictureBox.Image = System.Drawing.Image.FromFile("quiz.png"); // Letakkan file quiz.png di /bin/Debug/
+            //this.pictureBox.Location = new System.Drawing.Point(310, 120);
+            //this.pictureBox.Name = "pictureBox";
+            //this.pictureBox.Size = new System.Drawing.Size(180, 120);
+            //this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            //this.pictureBox.TabStop = false;
+            // 
+            // btnStartQuiz
+            // 
+            this.btnStartQuiz.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnStartQuiz.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStartQuiz.ForeColor = System.Drawing.Color.White;
+            this.btnStartQuiz.Location = new System.Drawing.Point(220, 270);
+            this.btnStartQuiz.Name = "btnStartQuiz";
+            this.btnStartQuiz.Size = new System.Drawing.Size(150, 45);
+            this.btnStartQuiz.Text = "Start Quiz";
+            this.btnStartQuiz.UseVisualStyleBackColor = false;
+            this.btnStartQuiz.Click += new System.EventHandler(this.btnStartQuiz_Click);
+            // 
+            // btnAddQuestion
+            // 
+            this.btnAddQuestion.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnAddQuestion.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAddQuestion.ForeColor = System.Drawing.Color.White;
+            this.btnAddQuestion.Location = new System.Drawing.Point(430, 270);
+            this.btnAddQuestion.Name = "btnAddQuestion";
+            this.btnAddQuestion.Size = new System.Drawing.Size(150, 45);
+            this.btnAddQuestion.Text = "Add Question";
+            this.btnAddQuestion.UseVisualStyleBackColor = false;
+            this.btnAddQuestion.Click += new System.EventHandler(this.btnAddQuestion_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.btnAddQuestion);
+            this.Controls.Add(this.btnStartQuiz);
+            this.Controls.Add(this.lblSubtitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QuizApp Main Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnStartQuiz;
+        private System.Windows.Forms.Button btnAddQuestion;
+    }
+}
---
+++ ConsoleApp2/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuizAppWinForms;
+
+namespace ConsoleApp2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartQuiz_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form = new QuizForm();
+            form.FormClosed += (s, args) => this.Close();
+            form.Show();
+        }
+
+        private void btnAddQuestion_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var addForm = new AddQuestion();
+            addForm.FormClosed += (s, args) => this.Close();
+            addForm.Show();
+        }
+
+    }
+}
