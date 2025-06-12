f89c5d41448f6907e81fd6570ab7fc97874d638a
NuhaAlfarhani <103867174+Aqilizaaz@users.noreply.github.com>
2025-06-12 22:06:49 +0700

    hasil score

---
+++ ConsoleApp2/ScoreFrom.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApp2
+{
+    partial class ScoreForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(0, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(800, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Hasil Quiz Kamu 🎉";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(80, 90);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(640, 80);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            // 
+            // ScoreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 220);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ScoreForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Skor Terakhir";
+            this.Load += new System.EventHandler(this.ScoreForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ ConsoleApp2/ScoreFrom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    public partial class ScoreForm : Form
+    {
+        private int latestCorrect;
+        private int latestTotal;
+        private DateTime latestTime;
+
+        public ScoreForm(int correct, int total)
+        {
+            InitializeComponent();
+            latestCorrect = correct;
+            latestTotal = total;
+            latestTime = DateTime.Now;
+        }
+
+        private void ScoreForm_Load(object sender, EventArgs e)
+        {
+            double persen = (double)latestCorrect / latestTotal * 100;
+
+            var display = new[]
+            {
+                new {
+                    Tanggal = latestTime.ToString("g"),
+                    Benar = latestCorrect,
+                    Total = latestTotal,
+                    Persentase = $"{persen:F1}%"
+                }
+            };
+
+            dataGridView.DataSource = display;
+        }
+    }
+}
