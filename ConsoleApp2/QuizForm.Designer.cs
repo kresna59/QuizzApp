bc44da48431a8294dcabbe088bd011ec3c9b65b3
NuhaAlfarhani <nuhaalfarhani@student.telkomuniversity.ac.id>
2025-06-12 21:53:58 +0700

    GUI Soal Quiz

---
+++ ConsoleApp2/QuizForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace QuizAppWinForms
+{
+    partial class QuizForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.RadioButton radio1;
+        private System.Windows.Forms.RadioButton radio2;
+        private System.Windows.Forms.RadioButton radio3;
+        private System.Windows.Forms.RadioButton radio4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.radio1 = new System.Windows.Forms.RadioButton();
+            this.radio2 = new System.Windows.Forms.RadioButton();
+            this.radio3 = new System.Windows.Forms.RadioButton();
+            this.radio4 = new System.Windows.Forms.RadioButton();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radio1
+            // 
+            this.radio1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.radio1.Location = new System.Drawing.Point(43, 89);
+            this.radio1.Name = "radio1";
+            this.radio1.Size = new System.Drawing.Size(429, 26);
+            this.radio1.TabIndex = 1;
+            this.radio1.TabStop = true;
+            this.radio1.Text = "Option 1";
+            this.radio1.UseVisualStyleBackColor = true;
+            // 
+            // radio2
+            // 
+            this.radio2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.radio2.Location = new System.Drawing.Point(43, 121);
+            this.radio2.Name = "radio2";
+            this.radio2.Size = new System.Drawing.Size(429, 26);
+            this.radio2.TabIndex = 2;
+            this.radio2.TabStop = true;
+            this.radio2.Text = "Option 2";
+            this.radio2.UseVisualStyleBackColor = true;
+            // 
+            // radio3
+            // 
+            this.radio3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.radio3.Location = new System.Drawing.Point(43, 156);
+            this.radio3.Name = "radio3";
+            this.radio3.Size = new System.Drawing.Size(429, 26);
+            this.radio3.TabIndex = 3;
+            this.radio3.TabStop = true;
+            this.radio3.Text = "Option 3";
+            this.radio3.UseVisualStyleBackColor = true;
+            // 
+            // radio4
+            // 
+            this.radio4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.radio4.Location = new System.Drawing.Point(43, 191);
+            this.radio4.Name = "radio4";
+            this.radio4.Size = new System.Drawing.Size(429, 26);
+            this.radio4.TabIndex = 4;
+            this.radio4.TabStop = true;
+            this.radio4.Text = "Option 4";
+            this.radio4.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnNext.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(394, 234);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(129, 35);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblQuestion.Location = new System.Drawing.Point(38, 40);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(507, 55);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Soal akan tampil di sini";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Gray;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(12, 9);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(77, 28);
+            this.btnBack.TabIndex = 6;
+            this.btnBack.Text = "Kembali ke Menu";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // QuizForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(557, 295);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.radio4);
+            this.Controls.Add(this.radio3);
+            this.Controls.Add(this.radio2);
+            this.Controls.Add(this.radio1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "QuizForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz App - Jawab Soal";
+            this.Load += new System.EventHandler(this.QuizForm_Load);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ ConsoleApp2/QuizForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ConsoleApp2;
+using QuizApp.Models;
+using QuizApp.Services;
+using QuizApp.Utils;
+
+namespace QuizAppWinForms
+{
+    public partial class QuizForm : Form
+    {
+        private List<Question> _questions;
+        private int _index = 0;
+        private int _score = 0;
+
+        public QuizForm()
+        {
+            InitializeComponent();
+        }
+
+        private void QuizForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var quizService = new QuizService(Config.QuestionBankPath);
+                _questions = quizService.GetQuestions();
+                if (_questions.Count == 0)
+                {
+                    MessageBox.Show("Belum ada soal.");
+                    Close();
+                    return;
+                }
+
+                DisplayQuestion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal memuat soal: " + ex.Message);
+                Close();
+            }
+        }
+
+        private void DisplayQuestion()
+        {
+            var q = _questions[_index];
+            lblQuestion.Text = $"{_index + 1}. {q.Text}";
+            radio1.Text = q.Options[0];
+            radio2.Text = q.Options[1];
+            radio3.Text = q.Options[2];
+            radio4.Text = q.Options[3];
+
+            ClearSelection();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (IsAnswerCorrect())
+                _score++;
+
+            _index++;
+
+            if (_index < _questions.Count)
+            {
+                DisplayQuestion();
+            }
+            else
+            {
+                ScoreService.SaveScore(_score, _questions.Count);
+                var scoreForm = new ScoreForm(_score, _questions.Count);
+                scoreForm.FormClosed += (s, args) => this.Close();
+                scoreForm.Show();
+            }
+        }
+
+        private bool IsAnswerCorrect()
+        {
+            int selectedIndex = -1;
+            if (radio1.Checked) selectedIndex = 0;
+            else if (radio2.Checked) selectedIndex = 1;
+            else if (radio3.Checked) selectedIndex = 2;
+            else if (radio4.Checked) selectedIndex = 3;
+
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("Silakan pilih salah satu jawaban.");
+                return false;
+            }
+
+            return _questions[_index].IsCorrect(selectedIndex);
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var main = new MainForm();
+            main.FormClosed += (s, args) => this.Close();
+            main.Show();
+        }
+
+        private void ClearSelection()
+        {
+            radio1.Checked = false;
+            radio2.Checked = false;
+            radio3.Checked = false;
+            radio4.Checked = false;
+        }
+    }
+}
